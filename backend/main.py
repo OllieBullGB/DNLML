6b9fc63d75a8ef7249327304f9017bc7b0af8fa1
Ollie Bull <59794969+OllieBullGB@users.noreply.github.com>
2023-11-04 23:20:43 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.staticfiles import StaticFiles
+from io import BytesIO
 from pydantic import BaseModel
 import pandas as pd
 from DAML import DAML
@@ -13,7 +14,7 @@ def read_root():
     return {"Hello, welcome to the DNLML API!"}
 
 @app.get("/link")
-def process(
+def processLink(
     task,
     dataset_url):
 
@@ -32,3 +33,28 @@ def process(
     except Exception as e:
         print(e)
         return {"error": "Invalid dataset URL"}
+
+@app.get("/file")
+def processFile(
+    task,
+    dataset_file: UploadFile = File(...)):
+
+    try:
+        print("TASK:", task)
+        print("DATASET FILE:", dataset_file)
+    except Exception as e:
+        print(e)
+        return {"error": "Invalid task or dataset file"}
+
+    try:
+        contents = dataset_file.file.read()
+        buffer = BytesIO(contents)
+        df = pd.read_csv(dataset_file)
+        buffer.close()
+        dataset_file.file.close()
+        print("GOT DATASET")
+        daml = DAML(df, task)
+        return {"type": daml.selected_model, "accuracy": daml.models[0][1], "url": daml.selected_model_url}
+    except Exception as e:
+        print(e)
+        return {"error": "Invalid dataset file"}
