9170463eb2964f80ada44bd972380a1f74d2cc99
Ollie Bull <59794969+OllieBullGB@users.noreply.github.com>
2023-11-05 02:36:51 +0000

    initial finished implementation of frontend

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,9 +3,11 @@
     <Disclaimer class="w-10/12 lg:w-1/2 mt-4 mx-auto"/>
     <ChatWindow class="w-10/12 h-96 flex-grow lg:w-1/2 mt-4 mx-auto rounded-2xl"/>
     <footer class=" bg-purple-300 w-full mx-auto flex h-32 justify-between p-4">
-      <input type="text" id="task" placeholder="Type here" class="input input-bordered w-full max-w-xs mx-auto" />
-      <input type="file" id="dataset" class="file-input w-full max-w-xs" />
-      <button @click="handleQuery" class="btn btn-success">Send</button>
+      <div class="flex flex-row justify-center w-full md:w-1/2 mx-auto">
+        <input type="text" id="task" placeholder="Type your query here" class="input input-bordered w-full max-w-xs mx-auto" />
+        <input type="file" id="dataset" class="file-input w-full max-w-xs" />
+        <button @click="handleQuery" class="btn btn-success">Send</button>
+      </div>
     </footer>
   </div>
 </template>
@@ -17,7 +19,13 @@ async function handleQuery()
     const chatWindow = document.getElementById("chat-window");
     const task = document.getElementById("task").value;
     //const dataset = document.getElementById("dataset").value;
-    const dataset = "https://firebasestorage.googleapis.com/v0/b/staffier-8863b.appspot.com/o/heart_failure_clinical_records_dataset.csv?alt=media&token=80ba957a-38e8-4c4c-80fa-e6c1324411d9&_gl=1*1ps84zf*_ga*MTI1MDU0NDI5Mi4xNjk5MTMzOTUw*_ga_CW55HF8NVT*MTY5OTEzMzk1MC4xLjEuMTY5OTEzNDAyMS41MS4wLjA."
+    let dataset = await fileToDataUrl(document.getElementById("dataset").files[0]);
+    // strip the data:text/csv;base64, from the beginning of the dataurl
+    dataset = dataset.substring(dataset.indexOf(",") + 1);
+
+    // pad out the dataset dataurl to be a multiple of 4
+    dataset = dataset.padEnd(dataset.length + (4 - dataset.length % 4) % 4, "=");
+
 
     // create a new text bubble from the right side
     const uChat = document.createElement("div");
@@ -25,7 +33,7 @@ async function handleQuery()
     uChat.classList.add("chat-end");
     const uBubble = document.createElement("div");
     uBubble.classList.add("chat-bubble");
-    uBubble.innerText = task;
+    uBubble.innerHTML = `${task}<br>Dataset: ${document.getElementById("dataset").files[0].name}`;
     uChat.appendChild(uBubble);
     chatWindow.appendChild(uChat);
 
@@ -34,16 +42,51 @@ async function handleQuery()
     sChat.classList.add("chat-start");
     const sBubble = document.createElement("div");
     sBubble.classList.add("chat-bubble");
-    sBubble.innerHTML= "Loading...";
+    sBubble.innerHTML= "<span class='loading loading-dots loading-lg'></span>";
     sChat.appendChild(sBubble);
     chatWindow.appendChild(sChat);
 
-    const res = await fetch(`127.0.0.1:8000/link?task="${task}"&dataset="${dataset}"`);
-    console.log(res);
+    try
+    {
+      const body = {task: task, dataset_file: dataset};
+      const options =
+      {
+        "method": "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: body
+      }
 
-    // using the data, create a new text bubble from the left side
-    // data contains "type", "accuracy", "url"
-    sBubble.innerHTML = `Model Type: ${res.type}<br>Accuracy: ${res.accuracy}<br>URL: ${res.url}`;
+      const res = await $fetch(`https://python-nlai-service.onrender.com/file`, options);
+      // link?task="${task}"&dataset="${dataset}"
+      // using the data, create a new text bubble from the left side
+      // data contains "type", "accuracy", "url"
+      if(res.type !== undefined)
+      {
+        sBubble.innerHTML = `Model Type: ${res.type}<br>Accuracy: ${(100 * res.accuracy)}%<br><button onclick="window.open('${res.url}', '_blank')" class="btn btn-wide mt-2">Download your tuned model</button>`;
+      }
+      else
+      {
+        sBubble.innerHTML = `I'm sorry, I didn't understand that. Could you please rephrase?`;
+      }
+    }
+    catch(err)
+    {
+      sBubble.innerHTML = `I'm sorry, I didn't understand that. Could you please rephrase?`;
+      console.log(err);
+    }
+    
+    
+}
+
+async function fileToDataUrl(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsDataURL(file)
+  })
 }
 
 </script>
--- frontend/components/ChatWindow.vue
+++ frontend/components/ChatWindow.vue
@@ -1,13 +1,8 @@
 <template>
     <div class="w-full h-full bg-gray-100 relative py-4 px-4" id="chat-window">
         <div class="chat chat-start">
-            <div class="chat-bubble">It's over Anakin, asdhashdalskhdlashdsaslhdhsd<br/>I have the high ground.</div>
+            <div class="chat-bubble">Hey, I'm DAML!<br>What questions can I help you with today?</div>
         </div>
-        <div class="chat chat-end">
-            <div class="chat-bubble">You underestimate my power!</div>
-        </div>
-
-        
     </div>
 </template>
 
